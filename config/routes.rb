aa3aac758d9d9517e3c98a0f40ba384644ccbd25
Walter van Woudenberg <vanwoudenbergwalter@gmail.com>
2016-06-14 16:06:02 +0200

    Made the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,58 +1,24 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
 
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  #as a user
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+  resources :profiles, only: [:show]
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+  resources :products, only: [:show] do
+    resource :appreciations, only: [:new, :create]
+  end
 
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
+  namespace :account do
+    resource :profile, only: [:show, :edit, :update]
 
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
+    resources :products do
+      resources :media
+    end
 
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+    resource :matches, only: [:index, :show] do
+      resource :messages, only: [:show, :create, :new]
+    end
+  end
 end
