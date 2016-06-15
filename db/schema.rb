45234faeef6902565ea093522dda226f5f4b6a6f
Jurjen Munneke <jurjenmunneke@me.com>
2016-06-15 15:51:18 +0200

    create navbar files

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615102648) do
+ActiveRecord::Schema.define(version: 20160615100323) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
