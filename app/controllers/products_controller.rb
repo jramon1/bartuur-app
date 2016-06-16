8dc91bb9e13613478288c469e7e122185b8ffe7a
jesse ramon <jramonibz555@gmail.com>
2016-06-16 17:44:32 +0200

    create appreciation action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,22 @@
 class ProductsController < ApplicationController
 
   def show
+    @product = Product.find(params[:id])
+
+    @appreciation_positive          = Appreciation.new
+    @appreciation_positive.product  = @product
+    @appreciation_positive.liked    = true
+
+    @appreciation_negative          = Appreciation.new
+    @appreciation_negative.product  = @product
+    @appreciation_negative.liked    = false
+
   end
 
   def product_params
     params.require(:product).permit(:name, :description, photos: [])
   end
+
+
 
 end
--- app/models/appreciation.rb
+++ app/models/appreciation.rb
@@ -2,4 +2,7 @@ class Appreciation < ActiveRecord::Base
   belongs_to :user
   belongs_to :product
   has_many :matches, dependent: :destroy
+  validates :liked, inclusion: { in: [true, false]  }
+  validates :product_id, presence: true
+  validates :user_id, presence: true
 end
