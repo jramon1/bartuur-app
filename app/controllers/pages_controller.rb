1d1ed6be58dadd6244dc889fee458e57ff37236f
Mike Hanot <mikehanot@gmail.com>
2016-06-16 12:21:13 +0200

    add the link and the redirect path according to the status

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,15 @@
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: :home
-  def home
-    si le user a un produit rediriger vers la page
-     show sinon redirige vers la creer un produit
 
-    if current_user.product_id = nill
-      redirect_to
+  def home
+    # si le user a un produit rediriger vers la page
+    #  show sinon redirige vers la creer un produit
+    if current_user
+      if current_user.products.empty?
+        redirect_to account_products_path
+      else
+        redirect_to product_path
+      end
+    end
   end
 end
