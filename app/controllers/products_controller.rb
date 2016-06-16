82c7f84bab8bbc0a3b965e70d8900ce86e301f4b
Mike Hanot <mikehanot@gmail.com>
2016-06-16 14:52:57 +0200

    add product controller before action

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,10 +1,16 @@
 class ProductsController < ApplicationController
+  before_action :find_product, only [:show]
 
   def show
   end
+
+  private
 
   def product_params
     params.require(:product).permit(:name, :description, photos: [])
   end
 
+  def find_product
+    @product = Product.find(params[:product_id])
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160615125606) do
+ActiveRecord::Schema.define(version: 20160616124911) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -95,7 +95,6 @@ ActiveRecord::Schema.define(version: 20160615125606) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
-    t.string   "profile_picture"
     t.string   "street"
     t.string   "zip_code"
     t.string   "city"
