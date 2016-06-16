68369219385452c18b9d087c6b8de694587216cc
Mike Hanot <mikehanot@gmail.com>
2016-06-16 11:39:41 +0200

    login page layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,10 @@
 class PagesController < ApplicationController
-
+  skip_before_action :authenticate_user!, only: :home
   def home
+    si le user a un produit rediriger vers la page
+     show sinon redirige vers la creer un produit
+
+    if current_user.product_id = nill
+      redirect_to
   end
 end
