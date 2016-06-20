6c0c5fa004a3a6ab2d1297c58923c4c413b78ab2
Mike Hanot <mikehanot@gmail.com>
2016-06-20 18:22:48 +0200

    add javascript to page 5 to hide and show the meu

---
+++ app/assets/javascripts/show_products.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+$('#carousel-container').hide(); //Initially form wil be hidden.
+
+ $('#product-description').click(function() {
+  $('#carousel-container').show();//Form shows on button click
+  $('#product-description').hide();
+  });
+
+ $('#go-back').click(function() {
+  $('#product-description').show();
+  $('#carousel-container').hide();
+  });
+});
+
