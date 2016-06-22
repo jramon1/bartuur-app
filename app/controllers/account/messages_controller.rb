c091381db880b754023129f4321a43a81d4ada54
jesse ramon <jramonibz555@gmail.com>
2016-06-22 11:29:47 +0200

    fix missing end in messages controller

--- app/controllers/account/messages_controller.rb
+++ app/controllers/account/messages_controller.rb
@@ -1,39 +1,40 @@
 class Account::MessagesController < ApplicationController
-before_action :set_conversation
+  before_action :set_conversation
 
   def index
-  @messages = @conversation.messages.all
-  update_read_status unless @messages.empty?
-  @message = @conversation.messages.new
-  @user = current_user
+    @messages = @conversation.messages.all
+    update_read_status unless @messages.empty?
+    @message = @conversation.messages.new
+    @user = current_user
   end
 
   def new
-  @message = Message.new(message_params)
+    @message = Message.new(message_params)
   end
 
   def create
-  @message.conversation_id = @conversation.id
-  @message.user_id = current_user.id
-  if @message.save!
-    redirect_to conversation_messages_path(@conversation)
+    @message.conversation_id = @conversation.id
+    @message.user_id = current_user.id
+    if @message.save!
+      redirect_to conversation_messages_path(@conversation)
 
+    end
   end
 
   def show
   end
 
 
-private
-def message_params
-  params.require(:message).permit(:body, :user_id)
-end
+  private
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
 
-def set_conversation
-  @conversation = Conversation.find(params[:conversation_id])
-end
+  def set_conversation
+    @conversation = Conversation.find(params[:conversation_id])
+  end
 
-def update_read_status
-  @messages.last.read = true if @messages.last.user_id != current_user.id
-end
+  def update_read_status
+    @messages.last.read = true if @messages.last.user_id != current_user.id
+  end
 end
