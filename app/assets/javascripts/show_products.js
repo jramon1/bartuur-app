0ac144759dd28828a898021aa80aa506dff51ac3
michael hanot <mikehanot@gmail.com>
2016-06-27 15:08:42 +0200

    add popover and fixed all little problem with the button

--- app/assets/javascripts/show_products.js
+++ app/assets/javascripts/show_products.js
@@ -1,15 +1,4 @@
-// $(document).ready(function() {
-
-// $('#carousel-container').hide(); //Initially form wil be hidden.
-
-//  $('#product-photo').click(function() {
-//   $('#carousel-container').show();//Form shows on button click
-//   $('#product-description').hide();
-//   });
-
-//  $('#go-back').click(function() {
-//   $('#product-description').show();
-//   $('#carousel-container').hide();
-//   });
-// });
+$(function () {
+  $('[data-toggle="popover"]').popover()
+})
 
